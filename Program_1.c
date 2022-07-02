29952ef03918a99c59f7060c0872df338f944040
Abhijeet Sagar <abhijeetspro39@gmail.com>
2022-07-03 01:44:19 +0530

    Given character is digit or not

---
+++ Program_1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+//given character is digit or not
+
+    char ch;
+    printf("Enter your character = ");
+    scanf("%c", &ch);
+
+    if(ch = 'a' && ch <= 'z'){
+        printf("Given digit is in lowercase character");
+    }
+    else if(ch = 'A' && ch <= 'Z'){
+        printf("Given digit is in UPPERCASE character");
+    }
+    else{
+        printf("Given digit is in NOT in character");
+    }
+
+
+    return 0;
+}
